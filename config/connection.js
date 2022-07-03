0d09f34b6ab2c5fbd13d8eda89d7c5614470d5f9
Richard Guarnieri <richard.gm@outlook.com>
2022-07-03 18:41:38 -0500

    added sequelize config

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,19 @@
-const { Sequelize } = require('sequelize');
 require('dotenv').config();
+const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize({
-    
-});
+const connectionURI = `mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}`
+
+const sequelize = new Sequelize(connectionURI);
+
+const init = async () => {
+    try {
+        await sequelize.sync()  
+        console.log('Connection has been established successfully.');
+    } catch (err) {
+        console.log('Unable to connect to the database:', err);
+    }
+} 
+
+init()
+
+module.exports = sequelize;
