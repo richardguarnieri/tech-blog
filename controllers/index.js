42d982bf1a2a35fe481451e78c4c19e061d1f9d5
Richard Guarnieri <richard.gm@outlook.com>
2022-07-06 12:33:51 -0500

    added dashboard hbs route

--- controllers/index.js
+++ controllers/index.js
@@ -4,10 +4,12 @@ const router = express.Router();
 const api = require('./api');
 const signin = require('./signin');
 const signup = require('./signup');
+const dashboard = require('./dashboard');
 
 router.use('/api', api);
 router.use('/signin', signin);
 router.use('/signup', signup);
+router.use('/dashboard', dashboard);
 
 router.get('/', (req, res) => {
     res.render('home');
