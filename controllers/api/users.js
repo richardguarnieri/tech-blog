c50f6c1a1a369e37ec66648df5d27a01519a4952
Richard Guarnieri <richard.gm@outlook.com>
2022-07-06 13:34:10 -0500

    fixed signup form

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -5,6 +5,12 @@ const User = require('./../../models/User');
 router.get('/', async (req, res) => {
     const response = await User.findAll();
     res.status(200).json(response);
+}
+)
+router.post('/', async (req, res) => {
+    // { } = req.body;
+    const response = await User.findAll();
+    res.status(200).json(response);
 })
 
 module.exports = router;
