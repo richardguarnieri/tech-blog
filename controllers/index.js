3108d66f25dfbc590ec4db2a30c399e40b49bc66
Richard Guarnieri <richard.gm@outlook.com>
2022-07-06 12:46:57 -0500

    added createPost hbs

--- controllers/index.js
+++ controllers/index.js
@@ -5,11 +5,13 @@ const api = require('./api');
 const signin = require('./signin');
 const signup = require('./signup');
 const dashboard = require('./dashboard');
+const createPost = require('./createPost');
 
 router.use('/api', api);
 router.use('/signin', signin);
 router.use('/signup', signup);
 router.use('/dashboard', dashboard);
+router.use('/createPost', createPost);
 
 router.get('/', (req, res) => {
     res.render('home');
