4192367760259317a63e10df5baa891fae596f25
Richard Guarnieri <richard.gm@outlook.com>
2022-07-07 00:11:16 -0500

    UX changes on the home hbs

--- public/js/index.js
+++ public/js/index.js
@@ -8,7 +8,8 @@ blogsEl.forEach((blog) => {
         let addCommentEl = event.currentTarget.nextElementSibling.nextElementSibling;
         // console.log(commentEl);
         // console.log(addCommentEl);
-        if (commentEl.classList.contains('hidden')) {
+        // commentEl.children.length != 0 to avoid displaying the section with no elements in the HTML - better UX
+        if (commentEl.classList.contains('hidden') && commentEl.children.length != 0) {
             commentEl.classList.remove('hidden');
         } else {
             commentEl.classList.add('hidden');
