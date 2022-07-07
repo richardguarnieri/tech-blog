0f989cca2e5c4c1a8be1eb2119d4535b3aff8f94
Richard Guarnieri <richard.gm@outlook.com>
2022-07-07 10:32:50 -0500

    got add comment route to work

--- controllers/api/comments.js
+++ controllers/api/comments.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const { Comment } = require('./../../models');
+const { BlogComment } = require('./../../models');
 
 router.post('/add', async (req, res) => {
-    const { content } = req.body;
+    const { content, blogId } = req.body;
     const userId = 1; // TO FIX
-    const response = await Comment.findAll();
-    res.status(200).json(response);
+    const response = await Comment.create({
+        content: content,
+        userId: userId
+    });
+    const responseParsed = JSON.parse(JSON.stringify(response));
+    console.log(responseParsed);
+    const blogComment = await BlogComment.create({
+        blogId: blogId,
+        commentId: responseParsed.id,
+    })
+    res.redirect('/');
 })
 
 module.exports = router;
