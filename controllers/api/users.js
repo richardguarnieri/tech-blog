09c2d4f50e27eb2ea97ea6176f2622647f71522f
Richard Guarnieri <richard.gm@outlook.com>
2022-07-07 18:53:00 -0500

    changed to frontEnd partly credential validation

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -1,37 +1,52 @@
 const express = require('express');
 const router = express.Router();
 const { User } = require('./../../models');
-
+ 
 router.post('/signin', async (req, res) => {
+    const message = 'Incorrect username or password, please try again!';
     try {
         console.log(req.body);
         const { username, password } = req.body;
         const response = await User.findOne({where: {username: username}});
-        console.log(response);
         if (!response) {
-            res.status(500).json({ message: 'Incorrect username or password, please try again' });
+            res.json({
+                success: false,
+                message: message
+            });
             return;
         }
         const isValidPassword = response.validatePassword(password);
         if (!isValidPassword) {
-            res.status(500).json({ message: 'Incorrect username or password, please try again' });
+            res.json({
+                success: false,
+                message: message
+            });
             return;
         }
-        res.redirect('/dashboard');
+        res.status(200).json({
+            success: true,
+        });
     } catch (err) {
         res.status(500).json(err.message);
     }
 })
 
 router.post('/signup', async (req, res) => {
+    const message = 'Credentials do not follow guidelines, please try again!';
     try {
         const { email, username, password } = req.body;
-        await User.create({
+        const response = await User.create({
             email, username, password
         });
-        res.redirect('/');
+        res.status(200).json({
+            success: true,
+        });
     } catch (err) {
-        res.status(500).json(err.message);
+        res.json({
+            success: false,
+            message: message,
+            error: err.message,
+        });
     }
 })
 
--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,80 @@
-const homeEl = document.querySelector('#home');
+const signinBtnEl = document.querySelector('#signin-btn')
+const signupBtnEl = document.querySelector('#signup-btn')
 const homeArticlesEl = document.querySelectorAll('.blog-home');
 const dashboardArticlesEl = document.querySelectorAll('.blog-dashboard')
 const deleteBtnEl = document.querySelector('#delete-btn')
 
-// console.log(dashboardArticlesEl);
+
+if (signinBtnEl) {
+    signinBtnEl.addEventListener('click', async () => {
+        const errorMsg = document.querySelector('#signin-error-msg');
+        const username = document.querySelector('#signin-username');
+        const password = document.querySelector('#signin-password');
+        const response = await fetch('/api/users/signin', {
+            method: 'POST',
+            body: JSON.stringify({
+                username: username.value,
+                password: password.value,
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        const data = await response.json()
+        if (data.success) {
+            window.location = '/dashboard'
+        } else {
+            username.value = '';
+            password.value = '';
+            errorMsg.innerHTML = data.message
+        }
+    })
+}
+
+if (signupBtnEl) {
+    signupBtnEl.addEventListener('click', async () => {
+        const errorMsg = document.querySelector('#signup-error-msg');
+        const email = document.querySelector('#signup-email');
+        const username = document.querySelector('#signup-username');
+        const password = document.querySelector('#signup-password');
+        const response = await fetch('/api/users/signup', {
+            method: 'POST',
+            body: JSON.stringify({
+                email: email.value,
+                username: username.value,
+                password: password.value,
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        const data = await response.json()
+        console.log(response);
+        if (data.success) {
+            window.location = '/'
+        } else {
+            email.value = '';
+            username.value = '';
+            password.value = '';
+            errorMsg.innerHTML = data.message
+        }
+    })
+}
+
+if (homeArticlesEl) {
+    homeArticlesEl.forEach((article) => {
+        article.addEventListener('click', (event) => {
+            const commentEl = event.currentTarget.nextElementSibling;
+            const addCommentEl = event.currentTarget.nextElementSibling.nextElementSibling;
+            // commentEl.children.length != 0 to avoid displaying the section with no elements in the HTML - better UX
+            if (commentEl.children.length != 0) {
+                commentEl.classList.toggle('active')
+            }
+            addCommentEl.classList.toggle('active');    
+        })
+    })
+}
+
 if (dashboardArticlesEl) {
     dashboardArticlesEl.forEach((article) => {
         article.addEventListener('click', (event) => {
@@ -12,8 +83,7 @@ if (dashboardArticlesEl) {
         });
     })
 }
-
-// console.log(document.querySelector('textarea[name=blogId]').innerHTML);
+;
 if (deleteBtnEl) {
     deleteBtnEl.addEventListener('click', async () => {
         await fetch('/api/blogs/delete', {
@@ -28,15 +98,3 @@ if (deleteBtnEl) {
         window.location = '/dashboard'
     })
 }
-
-homeArticlesEl.forEach((article) => {
-    article.addEventListener('click', (event) => {
-        const commentEl = event.currentTarget.nextElementSibling;
-        const addCommentEl = event.currentTarget.nextElementSibling.nextElementSibling;
-        // commentEl.children.length != 0 to avoid displaying the section with no elements in the HTML - better UX
-        if (commentEl.children.length != 0) {
-            commentEl.classList.toggle('active')
-        }
-        addCommentEl.classList.toggle('active');    
-    })
-})
