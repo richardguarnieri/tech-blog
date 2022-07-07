344cb9fc11f73981ae91017725c34cd0823ee812
Richard Guarnieri <richard.gm@outlook.com>
2022-07-07 10:52:55 -0500

    creating add post (blog) routes

--- controllers/api/blogs.js
+++ controllers/api/blogs.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const { Blog } = require('./../../models');
-const { Comment } = require('./../../models');
 
-router.get('/', async (req, res) => {
-    const response = await Blog.findAll({
-        include: Comment
+router.post('/create', async (req, res) => {
+    const { title, content } = req.body;
+    const userId = 2; // TO FIX
+    const response = await Blog.create({
+        title: title,
+        content: content,
+        userId: userId
     });
     console.log(response);
-    res.status(200).json(response);
+    res.redirect('/');
 })
 
 module.exports = router;
--- controllers/api/comments.js
+++ controllers/api/comments.js
@@ -4,19 +4,23 @@ const { Comment } = require('./../../models');
 const { BlogComment } = require('./../../models');
 
 router.post('/add', async (req, res) => {
-    const { content, blogId } = req.body;
-    const userId = 1; // TO FIX
-    const response = await Comment.create({
-        content: content,
-        userId: userId
-    });
-    const responseParsed = JSON.parse(JSON.stringify(response));
-    console.log(responseParsed);
-    const blogComment = await BlogComment.create({
-        blogId: blogId,
-        commentId: responseParsed.id,
-    })
-    res.redirect('/');
+    try {
+        const { content, blogId } = req.body;
+        const userId = 1; // TO FIX
+        const response = await Comment.create({
+            content: content,
+            userId: userId
+        });
+        const responseParsed = JSON.parse(JSON.stringify(response));
+        console.log(responseParsed);
+        const blogComment = await BlogComment.create({
+            blogId: blogId,
+            commentId: responseParsed.id,
+        })
+        res.redirect('/');
+    } catch (err) {
+        return;
+    }
 })
 
 module.exports = router;
