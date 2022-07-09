ddcdb9ca1f800615011908da091b588af161acd8
Richard Guarnieri <richard.gm@outlook.com>
2022-07-08 23:09:44 -0500

    fixed home route and type on dashboard view

--- controllers/home/home.js
+++ controllers/home/home.js
@@ -15,13 +15,6 @@ router.get('/', async (req, res) => {
             ]
         });
         const blogsParsed = JSON.parse(JSON.stringify(blogs));
-        
-        
-        const blog1 = blogsParsed[0];
-        console.log(new Date(blog1.createdAt).getFullYear());
-        
-        
-        
         res.render('home', {blogsParsed, loggedIn: req.session.loggedIn});
     } catch (err) {
         res.status(500).json({success: false, message: err.message})
