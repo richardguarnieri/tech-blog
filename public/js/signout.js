34b2a0ce80953f750e0df38ec3655c87102f6e49
Richard Guarnieri <richard.gm@outlook.com>
2022-07-08 21:42:52 -0500

    finished signout.js

--- public/js/signout.js
+++ public/js/signout.js
@@ -1,26 +1,12 @@
-const signupFormHandler = document.querySelector('form')
+const signoutBtn = document.querySelector('.signout-btn')
 
-signupFormHandler.addEventListener('submit', async (event) => {
-    event.preventDefault();
-    const errorMsg = document.querySelector('#signup-error-msg');
-    const email = document.querySelector('#signup-email');
-    const username = document.querySelector('#signup-username');
-    const password = document.querySelector('#signup-password');
-    const response = await fetch('/api/users/signup', {
+signoutBtn.addEventListener('click', async (event) => {
+    const response = await fetch('/api/users/signout', {
         method: 'POST',
-        body: JSON.stringify({
-            email: email.value.trim(),
-            username: username.value.trim(),
-            password: password.value.trim(),
-        }),
-        headers: {'Content-Type': 'application/json'}
     })
     if (response.ok) {
         window.location = '/'
     } else {
-        email.value = '';
-        username.value = '';
-        password.value = '';
-        errorMsg.innerHTML = 'Credentials do not follow guidelines, please try again!';
+        alert('Failed to log out.');
     }
 });
