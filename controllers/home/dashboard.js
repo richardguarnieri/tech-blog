7b69b1d5d0f9bfc9fd8bd0aada2056e820743f6c
Richard Guarnieri <richard.gm@outlook.com>
2022-07-08 22:33:49 -0500

    fully functional blog - pending date formatting

--- controllers/home/dashboard.js
+++ controllers/home/dashboard.js
@@ -5,18 +5,26 @@ const { Blog } = require('./../../models')
 const { User } = require('./../../models')
 
 router.get('/', async (req, res) => {
-    try {
-        // extract userId from session
-        const userId = req.session.userId;
-        const blogs = await Blog.findAll({
-            where: {userId},
-            include: User
-        });
-        const blogsParsed = JSON.parse(JSON.stringify(blogs));
-        res.render('dashboard', {blogsParsed, loggedIn: req.session.loggedIn});
-    } catch (err) {
-        res.status(500).json({success: false, message: err.message})
+    if (req.session.userId) {
+        try {
+            // extract userId from session
+            const userId = req.session.userId;
+            const blogs = await Blog.findAll({
+                where: {userId},
+                include: User
+            });
+            const blogsParsed = JSON.parse(JSON.stringify(blogs));
+            res.render('dashboard', {blogsParsed, loggedIn: req.session.loggedIn});
+        } catch (err) {
+            res.status(500).json({success: false, message: err.message})
+        }
+    } else {
+        try {
+            res.render('dashboard');
+        } catch (err) {
+            res.status(500).json({success: false, message: err.message})
+        }
     }
-});
+});    
 
 module.exports = router;
