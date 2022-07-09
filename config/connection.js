816af0d566e41a28bde67e0eacc8f9f7420ae67e
Richard Guarnieri <richard.gm@outlook.com>
2022-07-08 23:46:13 -0500

    added clearDB

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,10 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
+const herokuClearDbURL = 'mysql://ba460f1b1335b6:3b0ff9bd@us-cdbr-east-06.cleardb.net/heroku_6f39f4450240f29?';
+
 const connectionStringURI = `mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}`
 
-const sequelize = new Sequelize(connectionStringURI);
+const sequelize = new Sequelize(herokuClearDbURL);
 
 module.exports = sequelize;
