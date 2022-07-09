aa4b21de3f7693229fc01cc30b89c32a1d4f0a51
Richard Guarnieri <richard.gm@outlook.com>
2022-07-08 23:20:10 -0500

    added localName validation to home.js event listnr

--- public/js/home.js
+++ public/js/home.js
@@ -4,10 +4,13 @@ homeArticlesEl.forEach((article) => {
     article.addEventListener('click', (event) => {
         const commentEl = event.currentTarget.nextElementSibling;
         const addCommentEl = event.currentTarget.nextElementSibling.nextElementSibling;
+        console.log(addCommentEl);
         // commentEl.children.length != 0 to avoid displaying the section with no elements in the HTML - better UX
         if (commentEl.children.length != 0) {
             commentEl.classList.toggle('active')
         }
-        addCommentEl.classList.toggle('active');    
+        if (addCommentEl.localName === 'section') {
+            addCommentEl.classList.toggle('active');    
+        }
     })
 });
